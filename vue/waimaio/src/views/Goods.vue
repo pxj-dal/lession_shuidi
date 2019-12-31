aea350a1ddc26d7b35c0d682a87b3555f5181df5
pxj-dal <1365250749@qq.com>
2019-12-31 10:58:29 +0800

    weui组件

---
+++ vue/waimaio/src/views/Goods.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Goods
+    <shopcart/>
+    <cartcontrol/>
+    <food />
+  </div>
+</template>
+<script>
+//组件化
+import shopcart from '../components/shopcart/shopcart'
+import cartcontrol from '../components/cartcontrol/cartcontrol'
+import food from '../components/food/food'
+export default {
+  components:{
+
+  }
+  
+}
+</script>
