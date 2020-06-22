c44c6f826ce7f7b7f733fe57941713bc3c9846ab
pxj-dal <1365250749@qq.com>
2020-06-22 18:33:03 +0800

    Etag文件缓存及http响应

---
+++ vue/views/Search.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div class="container">
+        <i-input v-model="serchData" placeholder="输入你想查找的商品"
+        class="search" size="large">
+        <Button solt ="append" icon="ios-search" @click="search"/>
+        </i-input>
+        <Tag v-for="(item,index) in promotionTags" :key="index" >
+        {{item}}
+        </Tag>
+        </div>
+</div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            promotionTags:['卖2免1','领200券','199减100','母婴五折抢','充100送20'],
+            searchData:''
+        }
+    },
+    methods:{
+        search(){
+           console.log(this.searchData);
+          }
+
+    }
+}
+</script>
+<style >
+
+</style>
---
+++ vue/views/readme.md
@@ -0,0 +1,13 @@
+# learn-vue-components
+学习vue组件设计
+前提
+vue文档 MVVM 组件化思维
+1. vue 使用iview，element-ui,...vue开发框架，快速上手组件式开发。
+2. 分析特定业务场景，对专业组件进行分析
+3. 组件发布npm 学习一些开源组件
+
+
+- 电商网站搜索组件设计
+    1. searchbar  input + button
+    2. tags 功能 活动 广告上线
+    组件？<search />
